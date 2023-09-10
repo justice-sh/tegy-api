66b38487d86cdd571abbe341a56cfd4d09c1c4fc
Justice <sherllyj191@gmail.com>
2023-09-10 23:55:44 +0100

    Cleanup.

--- src/utilities/db.ts
+++ src/utilities/db.ts
@@ -1,16 +1,6 @@
-import { Client, Databases } from "node-appwrite"
 import { initializeApp, cert } from "firebase-admin/app"
 import { getFirestore, Timestamp, FieldValue, Filter } from "firebase-admin/firestore"
 import { config } from "../config/index.js"
-
-export const getDBClient = () => {
-  const client = new Client()
-    .setEndpoint(config.get("db_endpoint"))
-    .setProject(config.get("db_project_id"))
-    .setKey(config.get("db_project_key"))
-
-  return { db: new Databases(client), dbId: config.get<string>("db_id"), name: config.get<string>("db_name") }
-}
 
 export const getFirestoreDB = () => {
   const serviceAccount = JSON.parse(config.get("g_service_account"))
