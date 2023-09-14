f39faf07d7aea71c8db83fbb9c4b11abe026aebc
Justice <sherllyj191@gmail.com>
2023-09-14 17:01:19 +0100

    Finished /users route test

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -15,11 +15,59 @@ describe("/api/users", () => {
     server.close()
   })
 
-  describe("POST /", () => {
-    it("should return 400 if request param is invalid", async () => {
-      const res = await request(server).post("/api/users")
+  describe.only("POST /", () => {
+    let name = "",
+      password = "",
+      email = ""
+
+    const exec = () => request(server).post("/api/users").send({ name, password, email })
+
+    beforeEach(() => {
+      name = "user1"
+      password = "Password#1"
+      email = "me@gmail.com"
+    })
+
+    it("should return 400 if name is less than 3 characters", async () => {
+      name = "us"
+
+      const res = await exec()
 
       expect(res.status).toBe(400)
+    })
+
+    it("should return 400 if password is not valid", async () => {
+      password = "1234"
+
+      const res = await exec()
+
+      expect(res.status).toBe(400)
+    })
+
+    it("should return 400 if email is not valid", async () => {
+      email = "dkd"
+
+      const res = await exec()
+
+      expect(res.status).toBe(400)
+    })
+
+    it("should return 400 if user with email already exist", async () => {
+      await User.create({ name, email, password })
+
+      const res = await exec()
+
+      expect(res.status).toBe(400)
+    })
+
+    it("should create user if valid", async () => {
+      const res = await exec()
+
+      const [user] = await User.find({ email })
+
+      expect(res.status).toBe(200)
+      expect(res.body).toEqual(expect.objectContaining({ name, email, id: user.id }))
+      expect(res.body).not.toHaveProperty("password")
     })
   })
 
@@ -50,7 +98,7 @@ describe("/api/users", () => {
     })
 
     it("should return 404 if user with given ID was not found", async () => {
-      token = User.generateAuthToken({ id: "kdkdk" } as any)
+      token = User.generateAuthToken({ id: "lkdlddjldjdkddkd" } as any)
 
       const res = await exec()
 
