7c9ded3ac7c6ded8619e7c8543f2c50524e88ab4
Justice <sherllyj191@gmail.com>
2023-09-16 00:41:37 +0100

    Added test suite for authentication route.

---
+++ tests/integration/routes/auth.test.ts
@@ -0,0 +1,70 @@
+import { beforeEach, describe, expect, test } from "vitest"
+import request from "supertest"
+import bcrypt from "bcrypt"
+import { User } from "../../../src/models/user.js"
+import app from "../../../src/app.js"
+
+describe("/api/auth", () => {
+  let name = "",
+    email = "",
+    password = ""
+
+  beforeEach(async () => {
+    name = "Justice"
+    email = "authtest@gmail.com"
+    password = "Password#1"
+  })
+
+  const exec = () => request(app).post("/api/auth").send({ email, password })
+
+  test("should return 400 if email is invalid", async () => {
+    email = "me"
+
+    const res = await exec()
+
+    expect(res.status).toBe(400)
+  })
+
+  test("should return 400 if password is invalid", async () => {
+    password = "me"
+
+    const res = await exec()
+
+    expect(res.status).toBe(400)
+  })
+
+  test("should return 400 if user with email does not exist", async () => {
+    const res = await exec()
+
+    expect(res.status).toBe(400)
+  })
+
+  test("should return 400 if password is incorrect", async () => {
+    const salt = await bcrypt.genSalt(10)
+    const hashed = await bcrypt.hash(password, salt)
+
+    await User.create({ name, email, password: hashed })
+
+    password = "Password#2"
+
+    const res = await exec()
+
+    expect(res.status).toBe(400)
+    await User.clear()
+  })
+
+  test("should return valid JWT if request payload is valid", async () => {
+    const salt = await bcrypt.genSalt(10)
+    const hashed = await bcrypt.hash(password, salt)
+
+    const user = await User.create({ name, email, password: hashed })
+
+    const res = await exec()
+
+    const token = User.generateAuthToken(user)
+
+    expect(res.status).toBe(200)
+    expect(res.text).toBe(token)
+    await User.clear()
+  })
+})
