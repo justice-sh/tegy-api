fb144f81dfd6af0a2a061ea2a18be0f8d926cb8e
Justice <sherllyj191@gmail.com>
2023-09-24 04:28:52 +0100

    Refactored cors.

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -1,9 +1,7 @@
 import { Request, Response, NextFunction } from "express"
 
 export default function (req: Request, res: Response, next: NextFunction) {
-  if (process.env.APP_ENVIRONMENT === "devnet") {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  }
+  res.header("Access-Control-Allow-Origin", process.env.VALID_ORIGINS)
 
   next()
 }
